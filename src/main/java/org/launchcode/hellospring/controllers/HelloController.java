619eaa68818e90b1e081e0ff766d6b45502f6e4e
Zahon Matthews <zaematthews@gmail.com>
2022-08-26 19:23:31 -0400

    complteted hello spring project

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,41 +1,42 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
-    //Handles requests at path /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello () {
-//        return "Hello Spring!";
-//    }
-    // lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye () {
-        return "Goodbye Spring!";
-    }
-
     //Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-
-    public String helloWithQueryParam ( @RequestParam String name) {
-        return "Hello, " + name + "!";
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     //Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    @GetMapping("hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model) {
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
     }
     // lives at /hello/form
     @GetMapping("form")
     public String helloForm() {
         return "form";
 
+    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+       return "hello-list";
     }
 
 }
